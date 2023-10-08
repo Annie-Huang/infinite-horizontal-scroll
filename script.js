7831c3836cc13f7fac313df2ca4e67ca467a4d0b
Annie Huang <annie.hn@gmail.com>
2023-10-08 16:26:26 +1100

    Solution 2 to have continue infinite rolling to the left: Do exactly the solution 1 does but use the javascript to do the duplication.

--- script.js
+++ script.js
@@ -7,5 +7,20 @@ if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
 function addAnimation() {
   scrollers.forEach((scroller) => {
     scroller.setAttribute('data-animated', true);
+
+    const scrollerInner = scroller.querySelector('.scroller__inner');
+    // Very important to use Array.from. because it will take a copy of the original children. otherwise, each time we duplicate, scrollerInner.children's length will be double up
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      // console.log(duplicatedItem);
+      duplicatedItem.setAttribute(
+        'aria-hidden',
+        true
+      ); /* we don't want vision impair people to see this new list */
+
+      scrollerInner.appendChild(duplicatedItem);
+    });
   });
 }
